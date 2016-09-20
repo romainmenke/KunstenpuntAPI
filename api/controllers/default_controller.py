ddd75be688520a413cbea528789cb9a4fa51205b
Tom Ruette <tom@kunsten.be>
2016-09-20 10:31:28 +0200

    refactoring of muziekcentrum queries

--- api/controllers/default_controller.py
+++ api/controllers/default_controller.py
@@ -1,4 +1,4 @@
-from api.controllers.muziekcentrum import Identities, Organisations, Carriers
+from api.controllers.muziekcentrum import MCV, Identities, Organisations, Carriers
 from flask import jsonify
 
 
@@ -36,10 +36,44 @@ def releases_activiteit_id_get(activiteitId) -> str:
     return 'do some magic!'
 
 def releases_get(offset, limit, from_date = None, until_date = None, organiteit_id = None) -> str:
+#    if str(organiteit_id)[0] != mcv.db_id:
+#        organiteit_id = GET MCV ORGANITEIT UIT DATA.KUNSTEN.BE veld
+
     if from_date is None and until_date is None and organiteit_id is None:
         count, payload = mcv_carriers.get_full_list_of_carriers(limit, offset)
     elif from_date is not None and until_date is not None and organiteit_id is None:
         count, payload = mcv_carriers.get_list_of_carriers_in_period(from_date, until_date, limit, offset)
+    elif organiteit_id is not None and from_date is None and until_date is None:
+        local_id = str(organiteit_id)[2:]
+        count = 0
+        payload = []
+        if int(str(organiteit_id)[1]) == mcv_identities.table_id:
+            count_ma, payload_ma = mcv_carriers.get_list_of_carriers_by_identity_as_main_artist(local_id, limit, offset)
+            count += count_ma
+            if count_ma > 0:
+                payload.extend(payload_ma)
+            count_member, payload_member = mcv_carriers.get_list_of_carriers_by_identity_as_member(local_id, limit, offset)
+            count += count_member
+            if count_member > 0:
+                payload.extend(payload_member)
+            count_componist, payload_componist = mcv_carriers.get_list_of_carriers_by_identity_as_composer(local_id, limit, offset)
+            count += count_componist
+            if count_componist > 0:
+                payload.extend(payload_componist)
+        elif int(str(organiteit_id)[1]) == mcv_organisations.table_id:
+            count_label, payload_label = mcv_carriers.get_list_of_carriers_by_organisation_as_label(local_id, limit, offset)
+            count += count_label
+            if count_label > 0:
+                payload.extend(payload_label)
+            count_publisher, payload_publisher = mcv_carriers.get_list_of_carriers_by_organisation_as_publisher(local_id, limit, offset)
+            count += count_publisher
+            if count_publisher > 0:
+                payload.extend(payload_publisher)
+            count_distributor, payload_distributor = mcv_carriers.get_list_of_carriers_by_organisation_as_distributor(local_id, limit, offset)
+            count += count_distributor
+            if count_distributor > 0:
+                payload.extend(payload_distributor)
+        payload.sort(key=lambda x: x['datum'], reverse=True)
     else:
         count = -1
         payload = None
@@ -65,7 +99,6 @@ def podiumproducties_get(offset, limit, organiteitId = None, podiumProductieId =
 
 def podiumvoorstellingen_get(offset, limit, podiumproductieId = None, organiteitId = None, fromDate = None, untilDate = None) -> str:
     return 'do some magic!'
-
 
 mcv_identities = Identities()
 mcv_organisations = Organisations()
--- api/controllers/muziekcentrum.py
+++ api/controllers/muziekcentrum.py
@@ -29,10 +29,9 @@ class Carriers(MCV):
     def __init__(self):
         super(Carriers, self).__init__(3)
 
-    @staticmethod
-    def _make_list_payload_item(row):
+    def _make_list_payload_item(self, row):
         toonnaam = row[1] if row[2] is None else row[1] + '(' + row[2] + ')'
-        id = '13' + str(row[0])
+        id = str(self.db_id) + str(self.table_id) + str(row[0])
         payload_item = {
             "id": id,
             "toonnaam": toonnaam,
@@ -40,37 +39,148 @@ class Carriers(MCV):
         }
         return payload_item
 
-    def get_full_list_of_carriers(self, limit, offset):
-        sql = """SELECT ID, Title, VersionInfo, ReleaseDate
-                 FROM carriers
-                 ORDER BY ReleaseDate DESC
-                 LIMIT {0} OFFSET {1};""".format(str(limit), str(offset))
-
-        self.mcv_cur.execute(sql)
-        payload = [self._make_list_payload_item(row)for row in self.mcv_cur.fetchall()]
-
-        sql = "SELECT count(ID) FROM carriers;"
-        self.mcv_cur.execute(sql)
+    def _get_count_and_payload(self, sql_payload, sql_count):
+        self.mcv_cur.execute(sql_payload)
+        payload = [self._make_list_payload_item(row) for row in self.mcv_cur.fetchall()]
+        self.mcv_cur.execute(sql_count)
         count = self.mcv_cur.fetchone()[0]
-
         return count, payload
+
+    def get_full_list_of_carriers(self, limit, offset):
+        sql_payload = """SELECT ID, Title, VersionInfo, ReleaseDate
+                         FROM carriers
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {0} OFFSET {1};""".format(str(limit), str(offset))
+        sql_count = "SELECT count(ID) FROM carriers;"
+        return self._get_count_and_payload(sql_payload, sql_count)
 
     def get_list_of_carriers_in_period(self, from_date, until_date, limit, offset):
-        sql = """SELECT ID, Title, VersionInfo, ReleaseDate
-                 FROM carriers
-                 WHERE ReleaseDate > '{2}' AND ReleaseDate < '{3}'
-                 ORDER BY ReleaseDate DESC
-                 LIMIT {0} OFFSET {1};""".format(str(limit), str(offset), from_date, until_date)
+        sql_payload = """SELECT ID, Title, VersionInfo, ReleaseDate
+                         FROM carriers
+                         WHERE ReleaseDate > '{2}' AND ReleaseDate < '{3}'
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {0} OFFSET {1};""".format(str(limit), str(offset), from_date, until_date)
+        sql_count = """SELECT count(ID)
+                       FROM carriers
+                       WHERE ReleaseDate > '{0}' AND ReleaseDate < '{1}';""".format(from_date, until_date)
+        return self._get_count_and_payload(sql_payload, sql_count)
 
-        self.mcv_cur.execute(sql)
-        payload = [self._make_list_payload_item(row) for row in self.mcv_cur.fetchall()]
+    def get_list_of_carriers_by_identity_as_main_artist(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c
+                         INNER JOIN carriermainartists cm ON cm.CarrierID=c.ID
+                         WHERE cm.IdentityID={0} AND
+                               cm.ContextID IN (1,3,4,5,6) AND
+                               ((c.Filter & 2)=2) AND c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit), str(offset))
+        sql_count = """SELECT DISTINCT count(c.ID)
+                       FROM carriers c
+                       INNER JOIN carriermainartists cm ON cm.CarrierID=c.ID
+                       WHERE cm.IdentityID={0} AND
+                             cm.ContextID IN (1,3,4,5,6) AND
+                             ((c.Filter & 2)=2) AND c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
 
-        sql = """SELECT count(ID)
-                 FROM carriers
-                 WHERE ReleaseDate > '{2}' AND ReleaseDate < '{3}'
-                 ORDER BY ReleaseDate DESC
-                 LIMIT {0} OFFSET {1};""".format(str(limit), str(offset), from_date, until_date)
-        self.mcv_cur.execute(sql)
-        count = self.mcv_cur.fetchone()[0]
+    def get_list_of_carriers_by_identity_as_member(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c
+                         INNER JOIN carriermembers cm ON cm.CarrierID=c.ID
+                         WHERE cm.IdentityID={0} AND c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit), str(offset))
+        sql_count = """SELECT DISTINCT count(c.ID)
+                       FROM carriers c
+                       INNER JOIN carriermembers cm ON cm.CarrierID=c.ID
+                       WHERE cm.IdentityID={0} AND c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
 
-        return count, payload
+    def get_list_of_carriers_by_identity_as_composer(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c INNER JOIN carriermainartists cm ON cm.CarrierID=c.ID
+                         WHERE cm.IdentityID={0} AND
+                               cm.ContextID=2 AND
+                               ((c.Filter & 2)=2) AND c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit), str(offset))
+        sql_count = """SELECT DISTINCT count(c.ID)
+                       FROM carriers c INNER JOIN carriermainartists cm ON cm.CarrierID=c.ID
+                       WHERE cm.IdentityID={0} AND
+                             cm.ContextID=2 AND
+                             ((c.Filter & 2)=2) AND c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
+
+    def get_list_of_carriers_by_organisation_as_label(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c
+                         INNER JOIN dblinking l
+                         ON l.LinkType=3 AND
+                            l.ObjectID=c.ID AND
+                            l.ObjectType=5 AND
+                            l.LinkCategory=46
+                         WHERE l.LinkID={0} AND
+                               ((c.Filter & 2) = 2) AND
+                               c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit, str(offset)))
+        sql_count = """SELECT DISTINCT count(c.ID)
+                       FROM carriers c
+                       INNER JOIN dblinking l
+                       ON l.LinkType=3 AND
+                          l.ObjectID=c.ID AND
+                          l.ObjectType=5 AND
+                          l.LinkCategory=46
+                       WHERE l.LinkID={0} AND
+                             ((c.Filter & 2) = 2) AND
+                             c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
+
+    def get_list_of_carriers_by_organisation_as_publisher(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c
+                         INNER JOIN dblinking l
+                         ON l.LinkType=3 AND
+                            l.ObjectID=c.ID AND
+                            l.ObjectType=5 AND
+                            l.LinkCategory=24
+                         WHERE l.LinkID={0} AND
+                               ((c.Filter & 2) = 2) AND
+                               c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit), str(offset))
+        sql_count = """SELECT DISTINCT count(c.ID)
+                       FROM carriers c
+                       INNER JOIN dblinking l
+                       ON l.LinkType=3 AND
+                          l.ObjectID=c.ID AND
+                          l.ObjectType=5 AND
+                          l.LinkCategory=24
+                       WHERE l.LinkID={0} AND
+                             ((c.Filter & 2) = 2) AND
+                             c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
+
+    def get_list_of_carriers_by_organisation_as_distributor(self, organiteit_id, limit, offset):
+        sql_payload = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                         FROM carriers c
+                         INNER JOIN dblinking l
+                         ON l.LinkType=3 AND
+                            l.ObjectID=c.ID AND
+                            l.ObjectType=5 AND
+                            l.LinkCategory=23
+                         WHERE l.LinkID={0} AND
+                               ((c.Filter & 2) = 2) AND
+                               c.Hidden=0
+                         ORDER BY ReleaseDate DESC
+                         LIMIT {1} OFFSET {2}""".format(str(organiteit_id), str(limit), str(offset))
+        sql_count = """SELECT DISTINCT c.ID, c.Title, c.VersionInfo, c.ReleaseDate
+                       FROM carriers c
+                       INNER JOIN dblinking l
+                       ON l.LinkType=3 AND
+                          l.ObjectID=c.ID AND
+                          l.ObjectType=5 AND
+                          l.LinkCategory=23
+                       WHERE l.LinkID={0} AND
+                             ((c.Filter & 2) = 2) AND
+                             c.Hidden=0""".format(str(organiteit_id))
+        return self._get_count_and_payload(sql_payload, sql_count)
